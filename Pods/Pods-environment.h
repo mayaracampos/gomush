a6bd57ecb29b4be9f754cf05f732884d9d21fee0
mayaracampos <Mayara Campos>
2014-09-13 19:16:57 -0300

    ajuste pod

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,27 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// JSONKit
+#define COCOAPODS_POD_AVAILABLE_JSONKit
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 1.5pre.
+
+// Reachability
+#define COCOAPODS_POD_AVAILABLE_Reachability
+#define COCOAPODS_VERSION_MAJOR_Reachability 3
+#define COCOAPODS_VERSION_MINOR_Reachability 1
+#define COCOAPODS_VERSION_PATCH_Reachability 1
+
+// SAMGradientView
+#define COCOAPODS_POD_AVAILABLE_SAMGradientView
+#define COCOAPODS_VERSION_MAJOR_SAMGradientView 0
+#define COCOAPODS_VERSION_MINOR_SAMGradientView 2
+#define COCOAPODS_VERSION_PATCH_SAMGradientView 1
+
