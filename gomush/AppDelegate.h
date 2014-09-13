6e30ec8883966cb7b9d6460c5d8e83bc0b71de58
mayaracampos <Mayara Campos>
2014-09-12 21:47:48 -0300

    Initial Commit

---
+++ gomush/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gomush
+//
+//  Created by Mayara Campos on 9/12/14.
+//  Copyright (c) 2014 Mayara Campos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gomush/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  gomush
+//
+//  Created by Mayara Campos on 9/12/14.
+//  Copyright (c) 2014 Mayara Campos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
